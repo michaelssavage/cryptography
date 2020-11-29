8c03d10c2e82e6b723b8bd871497712fbb65796d
Michael S <33690140+michaelssavage@users.noreply.github.com>
2020-11-29 23:04:09 +0000

    example added

--- ModularExponentiation.java
+++ ModularExponentiation.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.math.BigInteger;
 
-public class ModularExponentiation {
+public class ModExpo {
 
     public static void main(String[] args)
     {
@@ -14,24 +14,27 @@ public class ModularExponentiation {
         BigInteger x = new BigInteger(in.next());
         BigInteger n = new BigInteger(in.next());
 
-        BigInteger result = modularExponent(a,x,n);
+        BigInteger result = modExp(a,x,n);
         System.out.println("The result is: " + result);
         
         in.close();
     }
-    // y = a ^ x (mod n)
-    private static BigInteger modularExponent(BigInteger base, BigInteger power, BigInteger modulo) {
-        //init y = 1
-        BigInteger result = BigInteger.ONE;
-        for(int i = 0; i < power.bitLength(); i++){
-            // check if exponent bit is odd
-            if (power.testBit(i)){
-                //y = (y * a) % n
-                result = (result.multiply(base)).mod(modulo);
+    // y = (a^x) % n, modular exponentiation function
+    private static BigInteger modExp(BigInteger a, BigInteger x, BigInteger n) {
+
+        // the square and multiply algorithm, right to left variant.
+
+        // init result (y) = 1                                         Example:(123^5) % 511
+        BigInteger y = BigInteger.ONE;                              // --------------------
+        for(int i = 0; i < x.bitLength(); i++){                     // |      |  a  |  y  |
+            // check if exponent bit is odd                            --------------------
+            if (x.testBit(i)){                                      // | init | 123 |  1  |
+                //result = (result * base) % modulus                   | x0=1 | 310 | 123 |
+                y = (y.multiply(a)).mod(n);                         // | x1=0 | 32  | 123 |
+            }                                                       // | x2=1 |  2  | 359 |
+            //base = (base^2) % modulus                                --------------------
+            a = (a.multiply(a)).mod(n);                             //     answer = 359
             }
-            //a = (a * a) % n
-            base = (base.multiply(base)).mod(modulo);
-            }
-        return result;
+        return y;
     }
 }
